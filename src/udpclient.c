6ee0696f2ca2a7a83d16860d54f1c1ad90725a38
auziabbruzzesi <austinta@udel.edu>
2018-05-04 19:39:55 -0400

    next up is to take parameters

--- src/udpclient.c
+++ src/udpclient.c
@@ -1,14 +1,14 @@
-/* udp_client.c */ 
+/* udp_client.c */
 /* Programmed by Adarsh Sethi */
 /* February 21, 2018 */
 
-#include <stdio.h>          /* for standard I/O functions */
-#include <stdlib.h>         /* for exit */
-#include <string.h>         /* for memset, memcpy, and strlen */
-#include <netdb.h>          /* for struct hostent and gethostbyname */
-#include <sys/socket.h>     /* for socket, sendto, and recvfrom */
-#include <netinet/in.h>     /* for sockaddr_in */
-#include <unistd.h>         /* for close */
+#include <stdio.h>      /* for standard I/O functions */
+#include <stdlib.h>     /* for exit */
+#include <string.h>     /* for memset, memcpy, and strlen */
+#include <netdb.h>      /* for struct hostent and gethostbyname */
+#include <sys/socket.h> /* for socket, sendto, and recvfrom */
+#include <netinet/in.h> /* for sockaddr_in */
+#include <unistd.h>     /* for close */
 #include <math.h>
 #define STRING_SIZE 1024
 
@@ -20,7 +20,7 @@ float ACKLossRate = 0;
 typedef struct{
   ////ACK//////
   short seq_num;
-}ACK;
+} ACK;
 
 // Struct for segment
 typedef struct{
@@ -29,58 +29,38 @@ typedef struct{
   short count;
   ///DATA///////
   char data[STRING_SIZE];
-}Segment;
+} Segment;
 
-   int SimulateLoss() {
-		int num = 0;
-		if (num<PacketLossRate) {
-		// Packet dropped
-			return 1; 
-		}
-		// Packet not dropped
-		return 0; 
-	}
-	
-	int SimulateACKLoss(){
-		int num = 0;
-		if (num<ACKLossRate) {
-		// ACK dropped
-			return 1;
-		}
-		// ACK not dropped
-		return 0; 
-	}   
+int main(void){
 
-int main(void) {
+  int sock_client; /* Socket used by client */
 
-   int sock_client;  /* Socket used by client */ 
-
-   struct sockaddr_in client_addr;  /* Internet address structure that
+  struct sockaddr_in client_addr; /* Internet address structure that
                                         stores client address */
-   unsigned short client_port;  /* Port number used by client (local port) */
+  unsigned short client_port;     /* Port number used by client (local port) */
 
-   struct sockaddr_in server_addr;  /* Internet address structure that
+  struct sockaddr_in server_addr;    /* Internet address structure that
                                         stores server address */
-   struct hostent * server_hp;      /* Structure to store server's IP
+  struct hostent *server_hp;         /* Structure to store server's IP
                                         address */
-   char server_hostname[STRING_SIZE]; /* Server's hostname */
-   unsigned short server_port;  /* Port number used by server (remote port) */
+  char server_hostname[STRING_SIZE]; /* Server's hostname */
+  unsigned short server_port;        /* Port number used by server (remote port) */
 
-   char filename[STRING_SIZE];  /* send message */
-   char modifiedfilename[STRING_SIZE]; /* receive message */
-   unsigned int msg_len;  /* length of message */
-   int bytes_sent, bytes_recd; /* number of bytes sent or received */
-  	int ACKLossRate = 0;
-  	int PacketLossRate = 0;
-  	
-   /* open a socket */
+  char filename[STRING_SIZE];         /* send message */
+  char modifiedfilename[STRING_SIZE]; /* receive message */
+  unsigned int msg_len;               /* length of message */
+  int bytes_sent, bytes_recd;         /* number of bytes sent or received */
+  int ACKLossRate = 0;
+  int PacketLossRate = 0;
 
-   if ((sock_client = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-      perror("Client: can't open datagram socket\n");
-      exit(1);
-   }
+  /* open a socket */
 
-   /* Note: there is no need to initialize local client address information
+  if ((sock_client = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
+    perror("Client: can't open datagram socket\n");
+    exit(1);
+  }
+
+  /* Note: there is no need to initialize local client address information
             unless you want to specify a specific local port.
             The local address initialization and binding is done automatically
             when the sendto function is called later, if the socket has not
@@ -88,150 +68,124 @@ int main(void) {
             The code below illustrates how to initialize and bind to a
             specific local port, if that is desired. */
 
-   /* initialize client address information */
+  /* initialize client address information */
 
-   client_port = 0;   /* This allows choice of any available local port */
+  client_port = 0; /* This allows choice of any available local port */
 
-   /* Uncomment the lines below if you want to specify a particular 
+  /* Uncomment the lines below if you want to specify a particular 
              local port: */
-   /*
+  /*
    printf("Enter port number for client: ");
    scanf("%hu", &client_port);
    */
 
-   /* clear client address structure and initialize with client address */
-   memset(&client_addr, 0, sizeof(client_addr));
-   client_addr.sin_family = AF_INET;
-   client_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* This allows choice of
+  /* clear client address structure and initialize with client address */
+  memset(&client_addr, 0, sizeof(client_addr));
+  client_addr.sin_family = AF_INET;
+  client_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* This allows choice of
                                         any host interface, if more than one 
                                         are present */
-   client_addr.sin_port = htons(client_port);
+  client_addr.sin_port = htons(client_port);
 
-   /* bind the socket to the local client port */
+  /* bind the socket to the local client port */
 
-   if (bind(sock_client, (struct sockaddr *) &client_addr,
-                                    sizeof (client_addr)) < 0) {
-      perror("Client: can't bind to local address\n");
-      close(sock_client);
-      exit(1);
-   }
+  if (bind(sock_client, (struct sockaddr *)&client_addr,sizeof(client_addr)) < 0){
+    perror("Client: can't bind to local address\n");
+    close(sock_client);
+    exit(1);
+  }
 
-   /* end of local address initialization and binding */
+  /* end of local address initialization and binding */
 
-   /* initialize server address information */
+  /* initialize server address information */
 
-   strcpy(server_hostname,"localhost" );
-   if ((server_hp = gethostbyname(server_hostname)) == NULL) {
-      perror("Client: invalid server hostname\n");
-      close(sock_client);
-      exit(1);
-   }
-   server_port = 45678;
+  strcpy(server_hostname, "localhost");
+  if ((server_hp = gethostbyname(server_hostname)) == NULL){
+    perror("Client: invalid server hostname\n");
+    close(sock_client);
+    exit(1);
+  }
+  server_port = 45678;
+
+  /* Clear server address structure and initialize with server address */
+  printf("here\n");
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  memcpy((char *)&server_addr.sin_addr, server_hp->h_addr,
+         server_hp->h_length);
+  server_addr.sin_port = htons(server_port);
+
+  /* user interface */
+
+  printf("Please input a filename:\n");
+  scanf("%s", filename);
+  msg_len = strlen(filename) + 1;
+
+  /* SEND FILE NAME */
+  bytes_sent = sendto(sock_client, filename, msg_len, 0,
+    (struct sockaddr *)&server_addr, sizeof(server_addr));
 
 
-   /* Clear server address structure and initialize with server address */
-   printf("here\n");
-   memset(&server_addr, 0, sizeof(server_addr));
-   server_addr.sin_family = AF_INET;
-   memcpy((char *)&server_addr.sin_addr, server_hp->h_addr,
-                                    server_hp->h_length);
-   server_addr.sin_port = htons(server_port);
 
-   /* user interface */
-   printf("Please input a filename:\n");
-   scanf("%s", filename);
-   msg_len = strlen(filename) + 1;
+  Segment s; 
+  ACK a;     
 
-   /* send message */
   
-   bytes_sent = sendto(sock_client, filename, msg_len, 0,
-            (struct sockaddr *) &server_addr, sizeof (server_addr));
+  int data_bytes = 0; //initialize data_bytes
+  short expectedSeqNum = 0;
+  short seq;
 
-   
-    Segment s; //to receive message into
-    ACK a; //to send to the server
+  printf("opening file\n");
+  FILE *file;
+  file = fopen("out.txt", "wb");
+  printf("entering rcv loop\n");
+  while (1){
+    // printf("before rcv\n");
+    bytes_recd = recvfrom(sock_client, &s, sizeof(s), 0, (struct sockaddr *)0, (int *)0);
+    //printf("after rcv\n");
+    // printf("line: %s\n",s.data);
+    //printf("line: %s",line);
+
+    //char * message = (char*)malloc((80)*sizeof(char)); //initialize the message... we know this will be at most 80 chars
+
+    seq = ntohs(s.seq_num); // pull the sequence number out of the header
+    char *message = (s.data);
+    printf("--------------------------------------\n");
+    printf("string: %s\n", message);
+    printf("sequence number of incoming packet %d\n", ntohs(s.seq_num));
+    printf("expected seqnum %d \n", expectedSeqNum);
+    printf("count: %d \n", s.count);
+
+    if (s.count == 0){
+      printf("received EOT packet\n");
+      break;
+    }
     
     
-        
-	size_t message_bytes = 1;
-	int data_bytes = 0;			//initialize data_bytes
-	short expectedSeqNum = 0;
-    short seq;
 
-
-    printf("opening file\n");
-    FILE * file;
-    file = fopen("out.txt","wb");
-    printf("entering rcv loop\n");
-   while(message_bytes) { 
-       // printf("before rcv\n");
-		bytes_recd = recvfrom(sock_client, &s, sizeof(s), 0,(struct sockaddr *) 0, (int *) 0);
-        //printf("after rcv\n");
-       // printf("line: %s\n",s.data);
-       //printf("line: %s",line);
-
-   		//char * message = (char*)malloc((80)*sizeof(char)); //initialize the message... we know this will be at most 80 chars
-        
-		seq = ntohs(s.seq_num); // pull the sequence number out of the header
-		char* message = (s.data);
-        printf("--------------------------------------\n");
-        printf("string: %s\n", message);
-        printf("sequence number of incoming packet %d\n",ntohs(s.seq_num));
-        printf("expected seqnum %d \n",expectedSeqNum);
-        printf("count: %d \n",s.count);
-
-  		if (s.count==0) {
-            printf("received EOT packet\n");
-   			break;
-   		}
-   		int pktrecv = SimulateLoss();
-   		if(0) {
-            
-   			continue;
-   		}
     
-   		
-   		//message_bytes = s.count; 	//will pop out of the loop if this is 0
-		
-		int ACK_Loss = SimulateACKLoss();
-		
-		if (s.seq_num==expectedSeqNum) {
-            printf("sequence number is good\n");
-			fprintf(file, "%s",message); 	//put the line in the out.txt file
-			a.seq_num = htons(expectedSeqNum);
-			
-			if (!ACK_Loss) { 
-                printf("sending ack\n");
-                printf("ack_num = %d\n",a.seq_num);
-				bytes_sent = sendto(sock_client, &a, sizeof(a), 0, (struct sockaddr *) &server_addr, sizeof (server_addr));
-			}
-		}
-		else {
-            printf("sequence number not good\n");
-			a.seq_num = htons(1 - expectedSeqNum);
-			if (!ACK_Loss) { 
-				bytes_sent = sendto(sock_client, &a, sizeof(a), 0, (struct sockaddr *) &server_addr, sizeof (server_addr));
-			}
-		}
-        printf("switching expectedseqnum\n");
-		expectedSeqNum = 1 - expectedSeqNum;
-	}
 
    
-   
- 
 
+    if (s.seq_num == expectedSeqNum){
+      printf("sequence number is good\n");
+      fprintf(file, "%s", message); //put the line in the out.txt file
+      a.seq_num = htons(expectedSeqNum);
 
-
-   /* get response from server */
-  
-//    printf("Waiting for response from server...\n");
-//    bytes_recd = recvfrom(sock_client, modifiedfilename, STRING_SIZE, 0,
-//                 (struct sockaddr *) 0, (int *) 0);
-//    printf("\nThe response from server is:\n");
-//    printf("%s\n\n", modifiedfilename);
-
-   /* close the socket */
-
-   close (sock_client);
+      if (!ACK_Loss){
+        printf("sending ack\n");
+        printf("ack_num = %d\n", a.seq_num);
+        bytes_sent = sendto(sock_client, &a, sizeof(a), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+      }
+    }else{
+      printf("sequence number not good\n");
+      a.seq_num = htons(1 - expectedSeqNum);
+      if (!ACK_Loss){
+        bytes_sent = sendto(sock_client, &a, sizeof(a), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+      }
+    }
+    printf("switching expectedseqnum\n");
+    expectedSeqNum = 1 - expectedSeqNum;
+  }
+  close(sock_client);
 }
--- src/udpserver.c
+++ src/udpserver.c
@@ -35,123 +35,122 @@ typedef struct{
 
 int main(void) {
 
-   int sock_server;  /* Socket on which server listens to clients */
 
-   struct sockaddr_in server_addr;  /* Internet address structure that
-                                        stores server address */
-   unsigned short server_port;  /* Port number used by server (local port) */
+  int sock_server;  /* Socket on which server listens to clients */
 
-   struct sockaddr_in client_addr;  /* Internet address structure that
-                                        stores client address */
-   unsigned int client_addr_len;  /* Length of client address structure */
+  struct sockaddr_in server_addr;  /* Internet address structure that
+                                      stores server address */
+  unsigned short server_port;  /* Port number used by server (local port) */
 
-   char filename[STRING_SIZE];  /* receive message */
-   char modifiedfilename[STRING_SIZE]; /* send message */
-   unsigned int msg_len;  /* length of message */
-   int bytes_sent, bytes_recd; /* number of bytes sent or received */
-   unsigned int i;  /* temporary loop variable */
+  struct sockaddr_in client_addr;  /* Internet address structure that
+                                      stores client address */
+  unsigned int client_addr_len;  /* Length of client address structure */
 
-   /* open a socket */
+  char filename[STRING_SIZE];  /* receive message */
+  char modifiedfilename[STRING_SIZE]; /* send message */
+  unsigned int msg_len;  /* length of message */
+  int bytes_sent, bytes_recd; /* number of bytes sent or received */
+  unsigned int i;  /* temporary loop variable */
 
-   if ((sock_server = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
-      perror("Server: can't open datagram socket\n");
-      exit(1);
-   }
+  /* open a socket */
 
-   /* initialize server address information */
+  if ((sock_server = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+    perror("Server: can't open datagram socket\n");
+    exit(1);
+  }
 
-   memset(&server_addr, 0, sizeof(server_addr));
-   server_addr.sin_family = AF_INET;
-   server_addr.sin_addr.s_addr = htonl (INADDR_ANY);  /* This allows choice of
-                                        any host interface, if more than one
-                                        are present */
-   server_port = SERV_UDP_PORT; /* Server will listen on this port */
-   server_addr.sin_port = htons(server_port);
+  /* initialize server address information */
 
-   /* bind the socket to the local server port */
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl (INADDR_ANY);  /* This allows choice of
+                                      any host interface, if more than one
+                                      are present */
+  server_port = SERV_UDP_PORT; /* Server will listen on this port */
+  server_addr.sin_port = htons(server_port);
 
-   if (bind(sock_server, (struct sockaddr *) &server_addr,
-                                    sizeof (server_addr)) < 0) {
-      perror("Server: can't bind to local address\n");
-      close(sock_server);
-      exit(1);
-   }
+  /* bind the socket to the local server port */
 
-   /* wait for incoming messages in an indefinite loop */
+  if (bind(sock_server, (struct sockaddr *) &server_addr,
+                                  sizeof (server_addr)) < 0) {
+    perror("Server: can't bind to local address\n");
+    close(sock_server);
+    exit(1);
+  }
 
-   printf("Waiting for incoming messages on port %hu\n\n", 
-                           server_port);
+  /* wait for incoming messages in an indefinite loop */
 
-   client_addr_len = sizeof (client_addr);
+  printf("Waiting for incoming messages on port %hu\n\n", 
+                          server_port);
+
+  client_addr_len = sizeof (client_addr);
+
+
+/* open file */
+  bytes_recd = recvfrom(sock_server, &filename, STRING_SIZE, 0,
+                  (struct sockaddr *) &client_addr, &client_addr_len);
+  printf("Received filename is: %s\n",filename);
+
 
   
-  /* open file */
-    bytes_recd = recvfrom(sock_server, &filename, STRING_SIZE, 0,
-                    (struct sockaddr *) &client_addr, &client_addr_len);
-    printf("Received filename is: %s\n",filename);
+  msg_len = bytes_recd;
+  FILE* file;                //initialize the file
+  file = fopen(filename,"r");
+  char * line = (char*)malloc(80*sizeof(char));
+  size_t buffer = STRING_SIZE;
+  
+  
+  if(file){
+  
+  /*Enter the FSM*/
+    short seq_num = 0;           
+    Segment s;
+    ACK a;
 
+  /*Enter outer loop*/
+    while(getline(&line,&buffer,file)>=0){
+      s.count = htons((short)strlen(line));
+      strcpy(s.data,line);
+      s.seq_num = seq_num;
+      printf("---------------------------\n");
+      printf("string: %s\n",line);
+      printf("sequence number of outgoing packet %d\n",s.seq_num);
+      printf("count %d\n",s.count);
+      
+      /*SEND SEGMENT*/
+      bytes_sent = sendto(sock_server,&s ,sizeof(s),0,
+        (struct sockaddr*) &client_addr, client_addr_len);
 
-    
-    msg_len = bytes_recd;
-    FILE* file;                //initialize the file
-    file = fopen(filename,"r");
-    
-    char * line = (char*)malloc(80*sizeof(char));
-    size_t buffer = STRING_SIZE;
-    
-    
-    if(file){
-/*Enter the FSM*/
-      short seq_num = 0;           
-      Segment s;
-      ACK a;
+      /*Enter inner loop: wait for Ack*/
 
-/*Enter outer loop*/
-
-      while(getline(&line,&buffer,file)>=0){
+      while(1){     
+        struct timeval timeout;
+        timeout.tv_sec = 10;
+        timeout.tv_usec = 0;
+        setsockopt(sock_server, SOL_SOCKET,SO_RCVTIMEO, (const void*)&timeout,sizeof(timeout));
+        bytes_recd = recvfrom(sock_server,&a,sizeof(a),0,(struct sockaddr *)0,(int*)0);
         
-        s.count = htons((short)strlen(line));
-        strcpy(s.data,line);
-        s.seq_num = seq_num;
-        printf("---------------------------\n");
-        printf("string: %s\n",line);
-        printf("sequence number of outgoing packet %d\n",s.seq_num);
-        printf("count %d\n",s.count);
-        
-        /*SEND SEGMENT*/
-        bytes_sent = sendto(sock_server,&s ,sizeof(s),0,
-          (struct sockaddr*) &client_addr, client_addr_len);
-
-        /*Enter inner loop: wait for Ack*/
-
-        while(1){     
-          struct timeval timeout;
-          timeout.tv_sec = 10;
-          timeout.tv_usec = 0;
-          setsockopt(sock_server, SOL_SOCKET,SO_RCVTIMEO, (const void*)&timeout,sizeof(timeout));
-          bytes_recd = recvfrom(sock_server,&a,sizeof(a),0,(struct sockaddr *)0,(int*)0);
-          
-          if(bytes_recd <= 0) {                     
-            printf("timeout.. retransmitting packet\n");
-            bytes_sent = sendto(sock_server,&s,1024,0,(struct sockaddr*) &client_addr, client_addr_len);
-            continue;
-          }
-          printf("ack recvd\n");
-          printf("expected ack number %d\n",seq_num);
-          printf("actual incoming ack number %d\n",ntohs(a.ack_num));
-          if(ntohs(a.ack_num) != seq_num){
-            continue;                     //null action
-          }else{
-            printf("changing seq_num\n");
-            seq_num = 1 - seq_num;
-            break;
-          }
+        if(bytes_recd <= 0) {         //timeout
+          printf("timeout.. retransmitting packet\n");
+          bytes_sent = sendto(sock_server,&s,1024,0,(struct sockaddr*) &client_addr, client_addr_len);
+          continue;
+        }
+        printf("ack recvd\n");
+        printf("expected ack number %d\n",seq_num);
+        printf("actual incoming ack number %d\n",ntohs(a.ack_num));
+        if(ntohs(a.ack_num) != seq_num){
+          continue;                     //null action
+        }else{
+          printf("changing seq_num\n");
+          seq_num = 1 - seq_num;
+          break;
         }
       }
-        Segment EOT;
-        EOT.count = 0;
-        EOT.seq_num = seq_num;
-        bytes_sent = sendto(sock_server,&EOT,1024,0,(struct sockaddr*) &client_addr, client_addr_len);
-      }   
+    }
+      Segment EOT;
+      EOT.count = 0;
+      EOT.seq_num = seq_num;
+      bytes_sent = sendto(sock_server,&EOT,1024,0,(struct sockaddr*) &client_addr, client_addr_len);
+  }   
 	
 }	
